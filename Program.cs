b6e795ed48c4d7a5a36eb2e8956406c10e88289d
Fadriano91 <fquimico@hotmail.com.br>
2022-03-31 17:55:46 -0300

    Confirm Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿var sucoL = 5;
+var sucoT = 5;
+
+Console.WriteLine("Escola o Suco: ");
+Console.WriteLine("L - Limão ");
+Console.WriteLine("T - Tamarindo");
+var opcaoSuco = (Console.ReadLine());
+
+if( opcaoSuco == "L"){ 
+    Console.WriteLine("Quantidade de suco de limao");
+    var qntL = Int32.Parse(Console.ReadLine());
+}else if(opcaoSuco =="T"){
+    Console.WriteLine("Quantidade de suco de tamarindo");
+    var qntT = Int32.Parse(Console.ReadLine());
+}
+
