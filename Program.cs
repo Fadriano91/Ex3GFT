07d7fceb55584dd391f50a8f312057ff7d6c6538
Fadriano91 <fquimico@hotmail.com.br>
2022-04-03 23:40:51 -0300

    Resolvendo e finalizando

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿var sucoL = 5;
-var sucoT = 5;
+﻿var sucoL = 10;
+var sucoT = 10;
 
 Console.WriteLine("Escola o Suco: ");
 Console.WriteLine("L - Limão ");
@@ -9,9 +9,35 @@ var opcaoSuco = (Console.ReadLine());
 if( opcaoSuco == "L"){ 
     Console.WriteLine("Quantidade de suco de limao");
     var qntL = Int32.Parse(Console.ReadLine());
+    if(qntL <= 2){
+        var totalL = (qntL * sucoL)* 0.97;
+        Console.WriteLine("Você teve desconto de 3% por litro!");
+        Console.WriteLine("Total a Pagar:" + totalL);
+    }else if(qntL >2 && qntL <= 5){
+        var totalL = (qntL * sucoL)* 0.96;
+        Console.WriteLine("Você teve desconto de 4% por litro!");
+        Console.WriteLine("Total a Pagar:" + totalL);
+    }else{
+        var totalL = (qntL * sucoL)* 0.95;
+        Console.WriteLine("Você teve desconto de 5% por litro!");
+        Console.WriteLine("Total a Pagar:" + totalL);
+    }
 }else if(opcaoSuco =="T"){
     Console.WriteLine("Quantidade de suco de tamarindo");
     var qntT = Int32.Parse(Console.ReadLine());
+    if(qntT <= 2){
+        var totalT = (qntT * sucoT)* 0.96;
+        Console.WriteLine("Você teve desconto de 4% por litro!");
+        Console.WriteLine("Total a Pagar:" + totalT);
+    }else if(qntT >2 && qntT <= 5){
+        var totalT = (qntT * sucoT)* 0.95;
+        Console.WriteLine("Você teve desconto de 5% por litro!");
+        Console.WriteLine("Total a Pagar:" + totalT);
+    }else{
+        var totalT = (qntT * sucoT)* 0.94;
+        Console.WriteLine("Você teve desconto de 6% por litro!");
+        Console.WriteLine("Total a Pagar:" + totalT);
+    }
 }
 
 //não consegui finalizar, tive um pouco de dificuldade pra iniciar
